ce399b511648ffb657e2d193f5f663f70761f9eb
sahithi <Sahithi>
2020-06-25 12:50:50 +0530

    changes added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,7 @@ class WelcomeController < ApplicationController
   def save_data
   	print params['name']
   	client = Client.create(:name=>params['name'] ,:pob=>params['place_of_birth'], :gender=>params['gender'] ,:dob=>params['date_of_birth'],:tob=>params['time_of_birth'],:contact_number=>params['contact_number'],:email=>params['email_id'],:profession=>params['profession'],:comments=>params['comments'])
+
+  	render  "welcome/thank_you.html.erb"
   end
 end
